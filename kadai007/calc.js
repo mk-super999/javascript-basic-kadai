b362e3d60f736590a8565ad2fa24e80d98c0ac1c
mk-super999 <138495449+mk-super999@users.noreply.github.com>
2023-08-16 15:36:37 +0900

    Add files via upload

---
+++ kadai007/calc.js
@@ -0,0 +1,31 @@
+let num = Math.floor(Math.random() * 16);
+console.log(num)
+
+if(num===3){
+    console.log('3の倍数です')
+}
+else if(num===5){
+    console.log('5の倍数です')
+}
+else if(num===5){
+    console.log('5の倍数です')
+}
+else if(num===6){
+    console.log('3の倍数です')
+}
+else if(num===9){
+    console.log('3の倍数です')
+}
+else if(num===12){
+    console.log('3の倍数です')
+}
+else if(num===15){
+    console.log('3と5の倍数です')
+}
+
+
+
+
+
+
+
