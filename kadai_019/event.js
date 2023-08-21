d92e36a81403a52eff974410d746ada6a27c9f1f
mk-super999 <138495449+mk-super999@users.noreply.github.com>
2023-08-21 21:32:14 +0900

    Add files via upload

---
+++ kadai_019/event.js
@@ -0,0 +1,7 @@
+const btnClk=document.getElementById('btn');
+const chengetext=document.getElementById('text');
+btnClk.addEventListener('click',()=>{
+    chengetext.textContent='ボタンをクリックしました';
+});
+
+
