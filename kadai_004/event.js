b828c1bad0bcf8d4e436a5cfbf0ead2827fcdbe6
mk-super999 <138495449+mk-super999@users.noreply.github.com>
2023-08-28 22:35:14 +0900

    Add files via upload

---
+++ kadai_004/event.js
@@ -0,0 +1,7 @@
+    $(window).on('load scroll', function(e) {
+    if(e.type === 'load'){
+    console.log('loadイベントが発生しました');}
+
+    if(e.type === 'scroll'){
+    console.log('scrollイベントが発生しました');
+    } });
